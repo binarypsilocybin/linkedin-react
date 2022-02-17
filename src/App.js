a4598f2168d4df673fb55a09a984108eaa92a9b0
binarypsilocybin <rogeriosvaldo@gmail.com>
2022-02-17 08:52:54 +0000

    new project course

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+function Header(props){
+  console.log(props)
+  return (
+    <header> 
+      <h1>{props.name}</h1>
+    </header>
+  )
+}
+
+function Main(props){ 
+  return(
+    <section>
+      <p>We serve the most {props.adjective} delicious food around.</p>
+    </section>
+  )
+}
+
+function Footer(){
+  return(
+    <footer>
+      <p>It's true.</p>
+    </footer>
+  )
+}
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header name="Cindy" />
+      <Main adjective="amazing" />
+      <Footer year={new Date().getFullYear} />
     </div>
   );
 }
