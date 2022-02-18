26a0e7ea6cc99771fca5ac433b0aa31fec625713
binarypsilocybin <rogeriosvaldo@gmail.com>
2022-02-18 19:01:27 +0000

    React route testing

--- src/App.js
+++ src/App.js
@@ -1,33 +1,26 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import './App.css';
+import { Routes, Route} from "react-router-dom";
+import{
+  Home,
+  About,
+  Events,
+  Contact
+} from "./pages";
 
-function App({login}) {
-  const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null)
 
-  useEffect(() => {
-    if(!login) return
-    setLoading(true);
-    fetch(`https://api.github.com/users/${login}`)
-    .then((response) => response.json())
-    .then(setData)
-    .then(()=> setLoading(false))
-    .catch(setError)
-  },[login])
 
-if(loading) return <h1>Loading...</h1>
-if(error) 
-  return <pre>{JSON.stringify(error, null, 2)}</pre>
-if(!data) return null;
-
-return (
-  <div>
-     <h1>{data.name}</h1>
-     <p>{data.location}</p>
-     <img alt={data.login} src={data.avatar_url} />
-   </div>
-);
+function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/events" element={<Events />} />
+        <Route path="/contact" element={<Contact />} />
+    </Routes>
+    </div>
+  )
 }
 
 export default App;
